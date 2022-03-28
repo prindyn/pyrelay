c5530682c37936f69514a8c01a2d1648660cdd1d
vasyl.prindyn <vasyl.prindyn@vakoms.com>
2022-03-28 03:49:52 +0300

    threads

--- app/networking.py
+++ app/networking.py
@@ -1,98 +1,165 @@
-import utime
 import network
-import usocket
 import machine
-import ubinascii
+import _thread
+import utime as time
+import usocket as socket
+import ubinascii as binascii
 from app.request import Request
 from app.request import Requitto
 from lib.umqtt.simple import MQTTClient
 
 
-class Network():
-    wlan = None
-    s_port = 80
-    s_host = '0.0.0.0'
-    username = '_prindyn'
-    password = '06011993'
-
-    def init(self) -> None:
-        pass
-
-    @classmethod
-    def do_connect(self):
-        self.wlan = network.WLAN(network.STA_IF)
-        self.wlan.active(True)
-        if not self.wlan.isconnected():
-            print('Connecting to network...')
-            self.wlan.connect(self.username, self.password)
-            while not self.wlan.isconnected():
-                pass
-            print('Network connected', self.wlan.ifconfig())
-            return True
-
-    @classmethod
-    def do_close(self):
-        if self.wlan and self.wlan.isconnected():
-            self.wlan.disconnect()
-            self.wlan.activate(False)
-        print('Network disconnected!')
-
-    @classmethod
-    def socket_open(self):
-        addr = usocket.getaddrinfo(self.s_host, self.s_port)[0][-1]
-        s = usocket.socket()
-        s.bind(addr)
-        s.listen(1)
-        print('Network listening on', addr)
-        while True:
-            request = []
-            conn, addr = s.accept()
-            print('client connected from', addr)
-            conn_file = conn.makefile('rwb', 0)
-            while True:
-                header = conn_file.readline()
-                if not header or header == b'\r\n':
-                    break
-                request.append(header.decode('utf-8'))
-            Request.process(request, conn)
-            conn.close()
-
-
-class mqtt():
+class ThreadedServer(object):
     client = None
-    port = 1883
-    server = '159.223.218.14'
-    client_id = ubinascii.hexlify(machine.unique_id())
-    topic_sub = b'/api/%s/#' % (client_id)
-    topic_pub = b'/relay/%s' % (client_id)
 
-    @classmethod
-    def connect_and_subscribe(self):
+    def __init__(self, host, port, thread_id=''):
+        self.host = host
+        self.port = port
+        self.id = thread_id if thread_id else binascii.hexlify(
+            '%s:%s' % (host, port)).decode('utf-8')
 
+    def sock(self):
+        self.type = 'sock'
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.client.bind((self.host, self.port))
+        self.client.listen(5)
+        return self
+
+    def mqtt(self, client_id='', topic_sub='', topic_pub=''):
+        self.type = 'mqtt'
+        self.client_id = client_id if client_id else binascii.hexlify(
+            machine.unique_id())
+        self.topic_sub = topic_sub.encode() if topic_sub else b'/api/%s/#' % (self.client_id)
+        self.topic_pub = topic_pub.encode() if topic_pub else b'/relay/%s' % (self.client_id)
+        self.client = MQTTClient(self.client_id, self.host, self.port)
+        return self
+
+    def listen(self):
+        _thread.allowsuspend(True)
+        if self.type == 'sock':
+            _thread.start_new_thread(
+                self.id, self.listenSockClient, [self.client])
+        if self.type == 'mqtt':
+            # _thread.start_new_thread(
+            # self.id, self.listenMqttClient, [self.client])
+            self.listenMqttClient(self.client)
+
+    def manage_ntf(self):
+        ntf = _thread.getnotification()
+        if ntf == -1:
+            return
+        elif ntf == 0:
+            _thread.lock()
+        elif ntf == 1:
+            _thread.unlock()
+        elif ntf == 7:
+            print({"nth_id": _thread.getSelfName()})
+
+    def listenSockClient(self, client):
+        while True:
+            client, address = self.client.accept()
+            print('Client connected from', address)
+            try:
+                conn_file = client.makefile('rwb', 0)
+                request = []
+                while True:
+                    header = conn_file.readline()
+                    if not header or header == b'\r\n':
+                        break
+                    request.append(header.decode('utf-8'))
+                Request.process(request, client)
+            except:
+                return False
+            client.close()
+
+    def listenMqttClient(self, client):
         def sub_cb(topic, msg):
             msg = msg.decode('utf-8')
             topic = topic.decode('utf-8')
-            print('ESP received %s from topic %s' % (msg, topic))
+            print('Received %s from topic %s' % (msg, topic))
             Requitto.process(topic, msg, self)
 
-        self.client = MQTTClient(self.client_id, self.server)
-        self.client.set_callback(sub_cb)
+        def restart_and_reconnect():
+            print('Failed to connect to MQTT broker. Reconnecting...')
+            time.sleep(10)
+            machine.reset()
+
+        def subscribe(client):
+            while True:
+                try:
+                    client.check_msg()
+                except OSError as e:
+                    restart_and_reconnect()
+
+        client.set_callback(sub_cb)
         try:
-            self.client.connect()
-            self.client.subscribe(self.topic_sub)
+            client.connect()
+            client.subscribe(self.topic_sub)
         except OSError as e:
             self.restart_and_reconnect()
-
         print('Connected to %s MQTT broker, subscribed to %s topic' %
-              (self.server, self.topic_sub))
+              (self.host, self.topic_sub))
         while True:
             try:
-                self.client.check_msg()
+                client.check_msg()
             except OSError as e:
-                self.restart_and_reconnect()
+                restart_and_reconnect()
 
-    @classmethod
-    def restart_and_reconnect(self):
-        print('Failed to connect to MQTT broker. Reconnecting...')
-        utime.sleep(10)
-        machine.reset()
+
+class Network(object):
+    client = None
+    essid = 'Relay-ESP32'
+    ipaddr = '192.168.0.1'
+
+    def __init__(self, password='', username='', mode='sta'):
+        self.mode = mode
+        self.username = username
+        self.password = password
+
+    def station(self):
+        self.client = network.WLAN(network.STA_IF)
+        self.client.active(True)
+        return self
+
+    def apoint(self, name='', ipaddr=''):
+        essid = name if name else self.essid
+        ipaddr = ipaddr if ipaddr else self.ipaddr
+        self.client = network.WLAN(network.AP_IF)
+        self.client.active(True)
+        self.client.config(essid=essid, password=self.password)
+        self.client.ifconfig(
+            (ipaddr, '255.255.255.0', '192.168.0.1', '8.8.8.8'))
+        return self
+
+    def connected(self):
+        if self.mode == 'ap':
+            status = self.client.active()
+        elif self.mode == 'sta':
+            status = self.client.isconnected()
+            if not status:
+                self.client.connect(self.username, self.password)
+        else:
+            status = False
+        return status
+
+    def connect(self):
+        tmo = 30
+        print('Connecting to network...')
+        try:
+            while not self.connected():
+                time.sleep(1)
+                tmo -= 1
+                if tmo == 0:
+                    print('Network connection failed')
+                    raise OSError
+            print('Network connected', self.client.ifconfig())
+            return self.client.ifconfig()
+        except:
+            return None
+
+    def close(self):
+        if self.client and self.connected():
+            self.client.disconnect()
+            self.client.activate(False)
+        print('Network disconnected')
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import gc
 from app.relay import Relay
-from app.networking import Network
-from app.networking import mqtt
+from app.networking import Network, ThreadedServer
 
+
+gc.collect()
 Relay.reset_states()
 
-if Network.do_connect():
-    mqtt.connect_and_subscribe()
+
+# connection = Network(mode='ap').apoint().connect()
+# if connection:
+    # ThreadedServer(connection[0], 80).sock().listen()
+connection = Network('06011993', '_prindyn').station().connect()
+if connection:
+    # ThreadedServer(connection[0], 80, 'web_socket').sock().listen()
+    ThreadedServer('159.223.218.14', 1883, 'mqtt_server').mqtt().listen()
